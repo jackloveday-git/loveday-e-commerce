b8fee2d44e1079c7583ca80be85a2e8196670998
jack.loveday95@gmail.com <jack.loveday95@gmail.com>
2021-10-30 00:09:44 -0400

    Added Product models functionality.

--- models/Product.js
+++ models/Product.js
@@ -6,12 +6,56 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // id: int, no null, primary, auto inc
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    // product_name: string, no null
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    // price: dec, no null, validation
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+
+      // Validate type here
+      validate: {
+        isDecimal: true
+      }
+    },
+
+    // stock: int, no null, default 10, validation
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+
+    // category_id: int, reference category id
+    category_id: {
+      type: DataTypes.INTEGER,
+
+      // Make reference here
+      references: {
+        model: "category",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
