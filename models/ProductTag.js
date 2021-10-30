87d5eb2339e0691cac88955fd62dfb9b8782ca5d
jack.loveday95@gmail.com <jack.loveday95@gmail.com>
2021-10-30 00:24:12 -0400

    Added ProductTag and Tag models.

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,43 @@
+// Product Tag Model by Jack Loveday
+
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
+// Define Product Tag Columns
 ProductTag.init(
   {
-    // define columns
+    // id: int, no null, primary, auto inc
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    // product_id: int, reference the product
+    product_id: {
+      type: DataTypes.INTEGER,
+
+      // Make the reference here
+      references: {
+        model: "product",
+        key: "id"
+      }
+    },
+
+    // tag_id: int, reference the tag
+    tag_id: {
+      type: DataTypes.INTEGER,
+
+      // Make the reference here
+      references: {
+        model: "tag",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,25 @@
+// Tag Model by Jack Loveday
+
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
-    // define columns
+    // id: int, no null, primary, auto inc
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    // tag_name: string
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
