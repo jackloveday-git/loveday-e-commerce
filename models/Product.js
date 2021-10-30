6d7fad973433506edd51f271d4393d01e14837c6
jack.loveday95@gmail.com <jack.loveday95@gmail.com>
2021-10-29 23:53:23 -0400

    Fixed models index.

--- models/Product.js
+++ models/Product.js
@@ -1,3 +1,5 @@
+// Product Model by Jack Loveday
+
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
--- models/index.js
+++ models/index.js
@@ -1,16 +1,32 @@
+// Index Model by Jack Loveday
+
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category, setup foreign key
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
-// Categories have many Products
+// Categories have many Products, setup foreign key
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag), setup foreign key
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag), setup foreign key
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
